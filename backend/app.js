a3d6a01c198f94bfa72b3d0d2da5eb21f1d5ca6e
haddiouiikram6-beep <haddiouiikram6@gmail.com>
2026-09-08 15:59:14 +0200

    Initial backend setup

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const PORT = 3000;
+app.get("/",(req, res)=>{
+    res.json({
+        message: "Carnet de Route API is running",
+    });
+});
+app.listen(PORT, () =>{
+console.log(`Server running on http://localhost:${PORT}`);
+
+});
